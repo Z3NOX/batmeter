b908acb57d2f2fcc003ce534793b04b4145031f7
Z3NOX <Z3NOX@users.noreply.github.com>
2020-09-25 00:27:19 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='batmeter',
+      version='0.1',
+      description='Notebook battery logging and plotting utility',
+      license="GPLv3"
+      author='z3nox',
+      author_email='zenox@dukun.de',
+      url='https://github.com/Z3NOX/batmeter',
+      install_requires=[
+          'tinydb>=4.1.1',
+          'matplotlib>=3.3.1'
+          ],
+      scripts=['batmeter'],
+     )
